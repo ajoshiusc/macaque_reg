b371760ff95805dae5a5790bc1f79146d86f53de
Anand A. Joshi <ajoshi@usc.edu>
2024-02-28 15:29:34 -0800

    update

--- main_afni_process_umd.py
+++ main_afni_process_umd.py
@@ -24,7 +24,7 @@ for t1 in sublist:
 
 
     cmd = f'@animal_warper -echo -input {t1r} -input_abbrev {subid} -base /home/ajoshi/MACAQUE_DEMO/NMT_v2.1_sym/NMT_v2.1_sym_05mm/NMT_v2.1_sym_05mm_SS.nii.gz -base_abbrev NMT2 -atlas_followers /home/ajoshi/MACAQUE_DEMO/NMT_v2.1_sym/NMT_v2.1_sym_05mm/CHARM_in_NMT_v2.1_sym_05mm.nii.gz /home/ajoshi/MACAQUE_DEMO/NMT_v2.1_sym/NMT_v2.1_sym_05mm/D99_atlas_in_NMT_v2.1_sym_05mm.nii.gz -atlas_abbrevs CHARM D99 -seg_followers /home/ajoshi/MACAQUE_DEMO/NMT_v2.1_sym/NMT_v2.1_sym_05mm/NMT_v2.1_sym_05mm_segmentation.nii.gz -seg_abbrevs SEG -outdir {outdir} -ok_to_exist'
-    #os.system(cmd)
+    os.system(cmd)
     print(cmd)
     
 
